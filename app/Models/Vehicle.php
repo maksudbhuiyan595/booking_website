7099f0af91066e4b8640b3c4b766c846bb450d2e
Kazi Omar Faruk <kaziomar@Kazis-MacBook-Air.local>
2025-11-27 21:38:14 +0600

    admin panel setup

---
+++ app/Models/Vehicle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vehicle extends Model
+{
+    protected $guarded = [];
+
+    protected $casts = [
+        'slabs' => 'array',
+        'has_extra_seat' => 'boolean',
+        'is_active' => 'boolean',
+    ];
+}
