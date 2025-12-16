d18e81b7706a26501269f946ef6f55a0b194e3c8
Maksud Bhuiyan <119837487+maksudbhuiyan595@users.noreply.github.com>
2025-12-16 23:31:37 +0600

    Update booking.blade.php

--- resources/views/frontend/pages/booking.blade.php
+++ resources/views/frontend/pages/booking.blade.php
@@ -71,7 +71,7 @@
 </section>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAocOxnI1fKkcZPIDH-ir2iw8y2kBqk-H4&libraries=places"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=&libraries=places"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <script>
